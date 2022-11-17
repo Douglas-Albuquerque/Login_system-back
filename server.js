587c761de66b82ccc317ae115da4313ce28c41a5
douglas-albuquerque <douglasalbuquerq@gmail.com>
2022-11-17 16:56:20 -0300

    att

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+import app from './src/app';
+
 const express = require("express");
 const app = express();
 
@@ -5,7 +7,7 @@ const port = process.env.PORT || 5000;
 
 //app.listen(5000);
 app.use("/", function (req, res) {
-  res.send("IT'S ALIVE");
+  res.send("hello");
 });
 
 
