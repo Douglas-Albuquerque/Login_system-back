f6d5b57a34b7526c895988ebb81d9b3e63c7d91f
douglas-albuquerque <douglasalbuquerq@gmail.com>
2022-11-17 12:42:50 -0300

    configurndo para upload no heroku

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 import app from './src/app';
 
+const express = require("express");
+const { resolve } = require("path");
+const app = express();
+const port = process.env.PORT || 5000;
+
 //app.listen(5000);
-app.listen(process.env.PORT || port, (err) => {
+app.get("/", function (req, res) {
+  res.send("hello");
+});
+
+
+app.listen(port, (err) => {
   if (err) {
     return console.log(err);
   }
